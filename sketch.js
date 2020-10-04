ee8471e12ca7c3b45ea1968644e2f5f34b7398ff
awesomeayaan13 <41842116+awesomeayaan13@users.noreply.github.com>
2020-10-03 22:01:43 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,52 @@
+var ball;
+
+var database,position;
+
+var ballPos
+
+function setup(){
+    database=firebase.database()
+    createCanvas(500,500);
+    ball = createSprite(250,250,10,10);
+    ball.shapeColor = "red";
+
+    ballPos=database.ref('Ball/Position')
+    ballPos.on("value",readposition,showerror)
+}
+
+function draw(){
+    background("white");
+    if(keyDown(LEFT_ARROW)){
+        writePosition(-1,0);
+    }
+    else if(keyDown(RIGHT_ARROW)){
+        writePosition(1,0);
+    }
+    else if(keyDown(UP_ARROW)){
+        writePosition(0,-1);
+    }
+    else if(keyDown(DOWN_ARROW)){
+        writePosition(0,+1);
+    }
+    drawSprites();
+}
+
+function writePosition(x,y){
+    database.ref('Ball/Position').set(
+        {
+            'x':position.x+x,
+            'y':position.y+y
+        }
+    )
+}
+
+
+function readposition(data){
+    position=data.val();
+    ball.x=position.x;
+    ball.y=position.y;
+}
+
+function showerror(){
+    console.log('error')
+}
